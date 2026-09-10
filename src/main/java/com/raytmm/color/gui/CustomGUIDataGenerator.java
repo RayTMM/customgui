beaaad8d01835a82535f7765e40085513e63a453
RayTMM <raytmmyt@gmail.com>
2026-09-10 01:20:48 -0400

    Created GitHub Repository

---
+++ src/main/java/com/raytmm/color/gui/CustomGUIDataGenerator.java
@@ -0,0 +1,11 @@
+package com.raytmm.color.gui;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomGUIDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
